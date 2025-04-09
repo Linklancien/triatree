b79018acedd3c581cc42b78f78f401608505812d
Linklancien <123779978+Linklancien@users.noreply.github.com>
2025-04-09 12:03:04 +0200

    Gravity (#1)
    * gravity for [x]
    
    * some gravity us working
    
    implement more testes
    
    * Sav before big change in mind
    
    * debug
    
    * test_check_reverse
    
    * testes ajoutées et fonctionnels
    
    * fix merge
    
    * changes
    
    * Update triatree.v
    
    * adding TODO for physic

--- triatree.v
+++ triatree.v
@@ -2,148 +2,153 @@ module main
 
 import math
 import math.vec
+import rand
 
 const triabase = [0, 1, 2, 3]
 
-type Self = Cara | Childs 
+type Self = Cara | Childs
 
 struct Triatree {
-	mut:
-	compo		Self	
-	
-	pos			[]int
-	dimensions	int		// 0 le plus petit
+mut:
+	compo Self
+
+	pos        []int
+	dimensions int
+	// 0 le plus petit
 }
 
 struct Childs {
-	mid	Triatree
-	up	Triatree
-	left	Triatree
-	right	Triatree
+	mid   Triatree
+	up    Triatree
+	left  Triatree
+	right Triatree
 }
 
 struct Cara {
 	// quantitées intensives
 }
 
-fn coo_tria_to_cart(pos []int, rota f32) vec.Vec2[f32]{
+fn coo_tria_to_cart(pos []int, rota f32) vec.Vec2[f32] {
 	mut position := vec.vec2[f32](0.0, 0.0)
 	mut angle := rota
-	for id in 0..pos.len{
+	for id in 0 .. pos.len {
 		n := pos.len - 1 - id
-		if pos[id] == 0{
+		if pos[id] == 0 {
 			angle += math.pi
-		}
-		else{
-			position += vec.vec2[f32](f32(math.pow(2, n)*math.sqrt(3)/6), 0).rotate_around_ccw(vec.vec2(f32(0), f32(0)), angle + f32(pos[id] - 1)*math.pi*2/3)
+		} else {
+			position += vec.vec2[f32](f32(math.pow(2, n) * math.sqrt(3) / 6), 0).rotate_around_ccw(vec.vec2(f32(0),
+				f32(0)), angle + f32(pos[id] - 1) * math.pi * 2 / 3)
+
 			// Distance a vérif
 		}
 	}
 	return position
 }
 
-fn coo_cart_to_tria(pos vec.Vec2[f32]) []int{
+fn coo_cart_to_tria(pos vec.Vec2[f32]) []int {
 	// to complete
-	panic("Not completed")
+	panic('Not completed')
 	return [0]
 }
 
-// neighbors
-fn neighbors(pos []int) [][]int{
-	n	:= pos.len
-	mut nei	:= [][]int{}
-	if pos[n-1] == 0{
+// NEIGHBORS:
+
+fn neighbors(pos []int) [][]int {
+	n := pos.len
+	mut nei := [][]int{}
+	if pos[n - 1] == 0 {
 		// 0 is the center of the triangle so it's neighbors can only be 1 2 3 of the same triangle
-		for i in 1..4{
-			nei << pos[..n-1]
-			nei[nei.len-1] << [i]
+		for i in 1 .. 4 {
+			nei << pos[..n - 1]
+			nei[nei.len - 1] << [i]
 		}
 		return nei
 	}
 
-	nei << pos[..n-1]
+	nei << pos[..n - 1]
 	nei[0] << [0]
-	mut to_ad := remove_from_base(triabase, [0, pos[n-1]])
+	mut to_ad := remove_from_base(triabase, [0, pos[n - 1]])
 
 	// au sein du même triangle, le triangle {1, 2, 3} sera toujours opposé au triangle [0]x{1, 2, 3}, pour la même valeur
 	// 1 out of 3
 	mut first_stop := -1
-	for tempo_id in 0..n{
-		id 		:= n - tempo_id - 1
-		if pos[id] != pos[n-1] && pos[id] != first_stop {
-			if pos[id] == 0 && to_ad.len == 2{
-				if pos == [0, 0, 1, 2]{panic(to_ad)}
+	for tempo_id in 0 .. n {
+		id := n - tempo_id - 1
+		if pos[id] != pos[n - 1] && pos[id] != first_stop {
+			if pos[id] == 0 && to_ad.len == 2 {
+				if pos == [0, 0, 1, 2] {
+					panic(to_ad)
+				}
 				nei << pos[..id]
-				nei[nei.len-1] << to_ad[0]
-				nei[nei.len-1] << []int{len: tempo_id, init: to_ad[1]}
-				
+				nei[nei.len - 1] << to_ad[0]
+				nei[nei.len - 1] << []int{len: tempo_id, init: to_ad[1]}
+
 				nei << pos[..id]
-				nei[nei.len-1] << to_ad[1]
-				nei[nei.len-1] << []int{len: tempo_id, init: to_ad[0]}
+				nei[nei.len - 1] << to_ad[1]
+				nei[nei.len - 1] << []int{len: tempo_id, init: to_ad[0]}
 				return nei
-			}
-			else{
+			} else {
 				// pos[id] appartient a {1, 2, 3}\{pos[n-1]}
-				if to_ad.len == 2{
+				if to_ad.len == 2 {
 					possible := remove_from_base(to_ad, [pos[id]])
-					if possible.len == 1{
+					if possible.len == 1 {
 						nei << pos[..id]
-						nei[nei.len-1] << [0]
-						nei[nei.len-1] << []int{len: tempo_id, init: possible[0]}
+						nei[nei.len - 1] << [0]
+						nei[nei.len - 1] << []int{len: tempo_id, init: possible[0]}
 
 						to_ad = remove_from_base(to_ad, possible)
 						first_stop = pos[id]
 					}
-				}
-				else if to_ad.len == 1{
+				} else if to_ad.len == 1 {
 					nei << pos[..id]
 					mut orientation := [0]
-					if pos[id] == 0{
-						orientation = remove_from_base(triabase, [0, pos[n-1], to_ad[0]])
+					if pos[id] == 0 {
+						orientation = remove_from_base(triabase, [0, pos[n - 1], to_ad[0]])
 					}
-					nei[nei.len-1] << orientation
+					nei[nei.len - 1] << orientation
 
 					new_base := remove_from_base(triabase, [pos[id], orientation[0]])
-					// new_base.len == 2 
-					for finition in (id + 1)..n{
-						nei[nei.len-1] << remove_from_base(new_base, [pos[finition]])
+
+					// new_base.len == 2
+					for finition in (id + 1) .. n {
+						nei[nei.len - 1] << remove_from_base(new_base, [pos[finition]])
 					}
-					
+
 					return nei
 				}
 			}
 		}
 	}
-	
+
 	return nei
 }
 
-fn hexa_world_neighbors(pos []int, current int) ([]int, [][]int){
-	if pos == []{
+fn hexa_world_neighbors(pos []int, current int) ([]int, [][]int) {
+	if pos == [] {
 		near := hexa_near_triangle(current)
 		return [near[0], near[2]], [[]int{}, []int{}]
 	}
 
 	mut directs_neighbors := neighbors(pos)
 
-	if directs_neighbors.len == 1{
-		if pos[0] == 1{
+	if directs_neighbors.len == 1 {
+		if pos[0] == 1 {
 			directs_neighbors << []int{len: pos.len, init: 1}
 			directs_neighbors << []int{len: pos.len, init: 1}
+
 			// the order doesn't mater because they are all [1, 1, ..., 1]
 			// this is the nearest of the center of the world
 			return hexa_near_triangle(current), directs_neighbors
 		}
 	}
-	if directs_neighbors.len <= 2{
+	if directs_neighbors.len <= 2 {
 		mut nei := []int{}
 		possible := [2, 3]
 		mut other := -1
-		for id in 0..pos.len{
-			if pos[id] == 1{
+		for id in 0 .. pos.len {
+			if pos[id] == 1 {
 				nei << [1]
-			}
-			else{
+			} else {
 				other = pos[id]
 				nei << remove_from_base(possible, [pos[id]])
 			}
@@ -151,109 +156,214 @@ fn hexa_world_neighbors(pos []int, current int) ([]int, [][]int){
 		directs_neighbors << nei
 		tria_nei := hexa_near_triangle(current)
 		mut near := [tria_nei[1]]
-		if other == 2{
+		if other == 2 {
 			near << [tria_nei[2]]
-		}
-		else if other == 3{
+		} else if other == 3 {
 			near << [tria_nei[1]]
 		}
-		return near , directs_neighbors
+		return near, directs_neighbors
 	}
+
 	// else{panic("A 0 without 3 neigbors in it's base ??? pos: ${pos} current: ${current}")}
 	// pos is inside a triangle
 	return []int{len: 3, init: current}, directs_neighbors
 }
 
-// find
-fn (tree Triatree) go_to(pos []int) &Triatree{
-	if pos == tree.pos{
+// follow the path in the triatree while the childs exist
+fn (tree Triatree) go_to(pos []int) &Triatree {
+	if pos == tree.pos {
 		return &tree
 	}
-	match tree.compo{
-		Childs{
-			if pos[0] == 0{
+	match tree.compo {
+		Childs {
+			if pos[0] == 0 {
 				return tree.compo.mid.go_to(pos[1..])
-			}
-			else if pos[0] == 1{
+			} else if pos[0] == 1 {
 				return tree.compo.mid.go_to(pos[1..])
-			}
-			else if pos[0] == 2{
+			} else if pos[0] == 2 {
 				return tree.compo.mid.go_to(pos[1..])
-			}
-			else if pos[0] == 3{
+			} else if pos[0] == 3 {
 				return tree.compo.mid.go_to(pos[1..])
 			}
 		}
-		else{}
+		else {}
 	}
 	return &tree
 }
 
-// divide & merge
-enum Changement {
-	divide
-	merge
-}
+// PHYSIC:
+// TODO: prendre en compte si les case sont occupé lorsque 2 cases peuvent être les suivantes
+// maybe change by adding a new fonction
 
-fn (mut tree Triatree) merge_divide(change Changement){
-	match change{
-		.divide{
-			if tree.dimensions > 0{
-				match tree.compo{
-					Cara{
-						mut pos_0 := tree.pos.clone()
-						pos_0 << [0]
-						mut pos_1 := tree.pos.clone()
-						pos_1 << [1]
-						mut pos_2 := tree.pos.clone()
-						pos_2 << [2]
-						mut pos_3 := tree.pos.clone()
-						pos_3 << [3]
-						tree.compo = Childs{
-							mid	:	Triatree{compo: tree.compo, pos: pos_0, dimensions: (tree.dimensions - 1)}
-							up	:	Triatree{compo: tree.compo, pos: pos_1, dimensions: (tree.dimensions - 1)}
-							left:	Triatree{compo: tree.compo, pos: pos_2, dimensions: (tree.dimensions - 1)}
-							right:	Triatree{compo: tree.compo, pos: pos_3, dimensions: (tree.dimensions - 1)}
-						}
-					}
-					else{}
+// take a position, and a corner toward is applied the gravity and return the next likely position
+fn gravity(pos []int, center int) []int {
+	n := pos.len
+
+	is_reverse := check_reverse(pos)
+
+	// next position:
+
+	// check if the current pos is at the gravity center
+	mut is_center := true
+	for i in 0 .. n {
+		id := n - i - 1
+		if pos[id] != center {
+			is_center = false
+			break
+		}
+	}
+
+	if is_center {
+		return pos
+	}
+
+	nei := neighbors(pos)
+	mut next := pos[..n - 1].clone()
+	if is_reverse {
+		if pos[n - 1] == 0 {
+			next_final_nei := remove_from_base(triabase, [0, center])
+			mut final_co := 0
+			if rand.bernoulli(0.5) or { false } {
+				final_co = next_final_nei[0]
+			} else {
+				final_co = next_final_nei[1]
+			}
+
+			// used to find the neighbor that end with the desired value
+			for elem in nei {
+				if elem[n - 1] == final_co {
+					next = elem.clone()
+					break
+				}
+			}
+		} else if pos[n - 1] == center {
+			next << 0
+		} else {
+			final_co := remove_from_base(triabase, [0, center, pos[n - 1]])[0]
+			for elem in nei {
+				if elem[n - 1] == final_co {
+					next = elem.clone()
+					break
 				}
 			}
 		}
-		.merge{
-			match tree.compo{
-				Childs{
-					// trouver parmis les childs si les cara sont les mêmes ?
+	} else {
+		if pos[n - 1] == 0 {
+			next << center
+		} else if pos[n - 1] == center {
+			next_final_nei := remove_from_base(triabase, [0, center])
+
+			if nei.len == 2 {
+				// in this case there is only one of the two neighbors wich is closer to the center of gravity
+				for elem in nei {
+					if elem[n - 1] == next_final_nei[0] || elem[n - 1] == next_final_nei[1] {
+						next = elem.clone()
+						break
+					}
 				}
-				else{}
+			} else {
+				mut final_co := 0
+				if rand.bernoulli(0.5) or { false } {
+					final_co = next_final_nei[0]
+				} else {
+					final_co = next_final_nei[1]
+				}
+
+				// used to find the neighbor that end with the desired value
+				for elem in nei {
+					if elem[n - 1] == final_co {
+						next = elem.clone()
+						break
+					}
+				}
 			}
+		} else {
+			next << 0
+		}
+	}
+
+	return next
+}
+
+// divide & merge:
+
+fn (mut tree Triatree) divide() {
+	if tree.dimensions > 0 {
+		match tree.compo {
+			Cara {
+				mut pos_0 := tree.pos.clone()
+				pos_0 << [0]
+				mut pos_1 := tree.pos.clone()
+				pos_1 << [1]
+				mut pos_2 := tree.pos.clone()
+				pos_2 << [2]
+				mut pos_3 := tree.pos.clone()
+				pos_3 << [3]
+				tree.compo = Childs{
+					mid:   Triatree{
+						compo:      tree.compo
+						pos:        pos_0
+						dimensions: (tree.dimensions - 1)
+					}
+					up:    Triatree{
+						compo:      tree.compo
+						pos:        pos_1
+						dimensions: (tree.dimensions - 1)
+					}
+					left:  Triatree{
+						compo:      tree.compo
+						pos:        pos_2
+						dimensions: (tree.dimensions - 1)
+					}
+					right: Triatree{
+						compo:      tree.compo
+						pos:        pos_3
+						dimensions: (tree.dimensions - 1)
+					}
+				}
+			}
+			else {}
 		}
 	}
 }
 
-// utilitary
-fn hexa_near_triangle(current int) []int{
-	if current == 0{
+// UTILITARY:
+// for the id of a triatree in a hexa world made of 6 triatree, return it's id surronded by the id of the triatree that are adjacent
+fn hexa_near_triangle(current int) []int {
+	if current == 0 {
 		return [5, 0, 1]
 	}
-	if current == 5{
+	if current == 5 {
 		return [4, 5, 0]
 	}
 	return [current - 1, current, current + 1]
 }
 
+// for a considered pos, return if the triangle is pointing upward -> true, or downward -> false by default all triangle are considered pointing downward
+fn check_reverse(pos []int) bool {
+	mut is_reverse := false
+
+	// len - 1 the last triangle doesn't interfet in it's own direction
+	for elem in pos[..pos.len - 1] {
+		if elem == 0 {
+			is_reverse = !is_reverse
+		}
+	}
+	return is_reverse
+}
+
 // very usefull:
-fn remove_from_base(base []int, liste []int) []int{
+fn remove_from_base(base []int, liste []int) []int {
 	mut final := []int{}
-	for elem in base{
+	for elem in base {
 		mut not_private := true
-		for privation in liste{
-			if elem == privation{
+		for privation in liste {
+			if elem == privation {
 				not_private = false
 				break
 			}
 		}
-		if not_private{
+		if not_private {
 			final << [elem]
 		}
 	}
--- triatree_test.v
+++ triatree_test.v
@@ -1,167 +1,279 @@
 module main
 
-// @[assert_continues]
 fn test_neighbors() {
-	println("--------------------")
-	println("test_neighbors():")
+	println('--------------------')
+	println('test_neighbors():')
 	mut tree := Triatree{}
+
 	// Check for a triangle in the center of the hugest triangle => all have 3 neighbors
-	print("test 1 center: ")
-	for x in 0..4{
-		for y in 0..4{
-			for z in 0..4{
+	print('test 1 center: ')
+	for x in 0 .. 4 {
+		for y in 0 .. 4 {
+			for z in 0 .. 4 {
 				// Check for the triangle in the center of a larger triangle
 				pos := [0, x, y, z]
 				nei := neighbors(pos)
+
 				// print de debug visuel:
 				// print(pos)
 				// print(" : ")
 				// println(nei)
 				// Only 3 triangle can be one's neighbor
-				assert nei.len == 3 , 'assertion 1 failed for [0, x, y, z]: ${pos}'
+				assert nei.len == 3, 'assertion 1 failed for [0, x, y, z]: ${pos}'
 				total := remove_from_base(triabase, [z])
 				mut ad := remove_from_base(triabase, [z])
-				for elem in nei{
-					assert elem[elem.len-1] != z , 'assertion 2 failed for [0, x, y, z]: ${pos}, elem[elem.len-1]: ${elem[elem.len-1]}'
-					assert elem.len == pos.len , 'assertion 3 failed for [0, x, y, z]: ${pos} : elem: ${elem}'
-					for is_added in total{
-						if is_added == elem[elem.len-1]{
-							ad = remove_from_base(ad, [elem[elem.len-1]])
+				for elem in nei {
+					assert elem[elem.len - 1] != z, 'assertion 2 failed for [0, x, y, z]: ${pos}, elem[elem.len-1]: ${elem[elem.len - 1]}'
+					assert elem.len == pos.len, 'assertion 3 failed for [0, x, y, z]: ${pos} : elem: ${elem}'
+					for is_added in total {
+						if is_added == elem[elem.len - 1] {
+							ad = remove_from_base(ad, [elem[elem.len - 1]])
 						}
 					}
 				}
+
 				// Check if this triangle has the other 3 coo in it's neighbor
-				assert ad.len == 0 , 'assertion 4 failed for [0, x, y, z]: ${pos} : ad: ${ad}'
+				assert ad.len == 0, 'assertion 4 failed for [0, x, y, z]: ${pos} : ad: ${ad}'
 			}
 		}
 	}
-	println("success")
-	print("test 2 edges: ")
+	println('success')
+	print('test 2 edges: ')
+
 	// Check for a the edge of a triangle on the edge of the hugest
-	for x in 1..4{
-		for y in 1..4{
+	for x in 1 .. 4 {
+		for y in 1 .. 4 {
 			pos := [x, y, y, y]
 			nei := neighbors(pos)
-			if x == y{
-				assert nei.len == 1 , 'assertion 1.1 failed for [0, x, y, z]: ${pos}'
-			}
-			else{
-				assert nei.len == 2 , 'assertion 1.2 failed for [0, x, y, z]: ${pos}'
+			if x == y {
+				assert nei.len == 1, 'assertion 1.1 failed for [0, x, y, z]: ${pos}'
+			} else {
+				assert nei.len == 2, 'assertion 1.2 failed for [0, x, y, z]: ${pos}'
 			}
 		}
 	}
-	println("success")
-	print("test 3 is nei of nei: ")
+	println('success')
+	print('test 3 is nei of nei: ')
+
 	// try := [[0, 1, 1, 2], [0, 0, 0, 0], [0, 1, 1, 1], [3, 1, 2, 3]]
 	// for elem in try{
-	for x in 0..4{
-		for y in 0..4{
-			for z in 0..4{
-				for t in 0..4{
+	for x in 0 .. 4 {
+		for y in 0 .. 4 {
+			for z in 0 .. 4 {
+				for t in 0 .. 4 {
 					elem := [x, y, z, t]
 					nei := neighbors(elem)
 					mut is_nei := [][]int{}
-					for to_check in nei{
+					for to_check in nei {
 						test := neighbors(to_check)
+
 						// println("")
 						// print("$to_check: ")
 						// println(test)
-						for current in test{
-							if current == elem{
+						for current in test {
+							if current == elem {
 								is_nei << to_check
 								break
 							}
 						}
 					}
-					assert is_nei.len == nei.len , 'assertion 1 failed for [...] = ${elem}: \n $is_nei alors que $nei'
+					assert is_nei.len == nei.len, 'assertion 1 failed for [...] = ${elem}: \n ${is_nei} alors que ${nei}'
 				}
 			}
 		}
 	}
-	println("success")
-	println("Passed")
-	println("--------------------")
+	println('success')
+	println('Passed')
+	println('--------------------')
 }
 
 fn test_hexa_world_neighbors() {
-	println("--------------------")
-	println("test_hexa_world_neighbors():")
+	println('--------------------')
+	println('test_hexa_world_neighbors():')
 
-	print("test 1 specifics cases: ")
+	print('test 1 specifics cases: ')
 	near_null, nei_null := hexa_world_neighbors([]int{}, 1)
-	assert near_null.len == 2	, 'assertion failed for specific: []'
-	assert near_null[0]	 != near_null[1] 	, 'assertion failed for specific: []'
-	assert nei_null.len  == 2	, 'assertion failed for specific: []'
-	println("success")
+	assert near_null.len == 2, 'assertion failed for specific: []'
+	assert near_null[0] != near_null[1], 'assertion failed for specific: []'
+	assert nei_null.len == 2, 'assertion failed for specific: []'
+	println('success')
 
-	print("test 2 insides cases: ")
-	for x in 0..4{
-		for y in 0..4{
+	print('test 2 insides cases: ')
+	for x in 0 .. 4 {
+		for y in 0 .. 4 {
 			pos := [0, x, y]
 			near, nei := hexa_world_neighbors(pos, 1)
-			assert near.len == 3	, 'assertion failed for specific: []'
-			assert nei.len  == 3	, 'assertion failed for specific: []'
+			assert near.len == 3, 'assertion failed for specific: []'
+			assert nei.len == 3, 'assertion failed for specific: []'
 			also_nei := neighbors(pos)
-			assert nei ==  also_nei , 'assertion failed for specific: [] \n $nei != $also_nei'
+			assert nei == also_nei, 'assertion failed for specific: [] \n ${nei} != ${also_nei}'
 		}
 	}
-	println("success")
-	print("test 3 edge cases: ")
-	for cur in 1..7{
-		for t in 1..3{
-			for aff in [1, t]{
-				for aff_sec in [1, t]{
+	println('success')
+	print('test 3 edge cases: ')
+	for cur in 1 .. 7 {
+		for t in 1 .. 3 {
+			for aff in [1, t] {
+				for aff_sec in [1, t] {
 					pos := [t, aff, aff_sec]
 					near, nei := hexa_world_neighbors(pos, 1)
 					mut count := 0
-					for n in near{
-						if n != cur{
+					for n in near {
+						if n != cur {
 							count += 1
 						}
 					}
-					assert count >= 1 , 'assertion failed for edge: $pos \n $near == $cur'
+					assert count >= 1, 'assertion failed for edge: ${pos} \n ${near} == ${cur}'
 				}
 			}
 		}
 	}
-	println("success")
+	println('success')
 
-	println("Passed")
-	println("--------------------")
+	println('Passed')
+	println('--------------------')
 }
 
-@[assert_continues]
-fn test_remove_from_base() {
-	println("--------------------")
-	println("test_private():")
+fn test_gravity() {
+	println('--------------------')
+	println('test_gravity():')
+	print('test 1 special case: ')
+	assert gravity([0], 1) == [1], 'assertion failed for specific: [0]'
+	assert gravity([1], 1) == [1], 'assertion failed for specific: [1]'
+	assert gravity([2], 1) == [0], 'assertion failed for specific: [2]'
+	assert gravity([3], 1) == [0], 'assertion failed for specific: [3]'
+	println('success')
 
-	print("test 1 usefull: ")
-	base := [0, 1, 2, 3]
-	assert remove_from_base(base, [0]) == [1, 2, 3] , 'assertion failed for specific: [0]'
-	assert remove_from_base(base, [1]) == [0, 2, 3] , 'assertion failed for specific: [1]'
-	assert remove_from_base(base, [2]) == [0, 1, 3] , 'assertion failed for specific: [2]'
-	assert remove_from_base(base, [3]) == [0, 1, 2] , 'assertion failed for specific: [3]'
-
-	for i in 0..5{
-		assert remove_from_base(base, base[..i]).len == base.len - i , 'assertion failed for i: ${i}'
+	print('test 2 one [x], differents centers: ')
+	for center in 1 .. 3 {
+		assert gravity([0], center) == [center], 'assertion failed for specific: [0], center: ${center}'
+		assert gravity([center], center) == [center], 'assertion failed for specific: [center], center: ${center}'
+		others := remove_from_base([1, 2, 3], [center])
+		assert gravity([others[0]], center) == [0], 'assertion failed for specific: [0], center: ${center}, others: ${others[0]}'
+		assert gravity([others[1]], center) == [0], 'assertion failed for specific: [0], center: ${center}, others: ${others[1]}'
 	}
-	assert remove_from_base(base, []).len == 4 , 'assertion failed for empty []'
-	assert remove_from_base(base, [5, 6, 7]).len == 4 , 'assertion failed for non in base [5, 6, 7]'
-	assert remove_from_base(base, [3, 3]).len == 3 , 'assertion failed for repetitive: [3, 3]'
-	println("success")
+	println('success')
 
-	print("test 2 extension: ")
+	print('test 3 more [x, y, z]: ')
+	for center in 1 .. 3 {
+		for x in 0 .. 3 {
+			for y in 0 .. 3 {
+				for z in 0 .. 3 {
+					pos := [x, y, z]
+					is_reverse := check_reverse(pos)
+					next := gravity(pos, center)
+
+					assert next.len == pos.len, 'assertion failed for lens : pos: ${pos}, next: ${next}, center: ${center}'
+
+					if z == center {
+						if z == x && z == y {
+							assert next == pos, 'assertion failed z center pos: ${pos}, next: ${next}, center: ${center}, also know as the extrem position'
+						} else if is_reverse {
+							assert next == [x, y, 0], 'assertion failed z center pos: ${pos}, next: ${next}, center: ${center}, is_reverse: ${is_reverse}'
+						} else {
+							mut count := 0
+							for test in remove_from_base([1, 2, 3], [center]) {
+								if next[next.len - 1] == test {
+									count += 1
+								}
+							}
+							assert count == 1, 'assertion failed z center pos: ${pos}, next: ${next}, center: ${center}, is_reverse: ${is_reverse}'
+						}
+					} else {
+						// z isn't the center here
+						for test in remove_from_base([1, 2, 3], [center]) {
+							if z == test {
+								if is_reverse {
+									assert next[next.len - 1] == remove_from_base([1, 2, 3],
+										[z, center])[0], 'assertion failed pos: ${pos}, next: ${next}, center: ${center}, is_reverse: ${is_reverse}'
+									break
+								} else {
+									assert next == [x, y, 0], 'assertion failed pos: ${pos}, next: ${next}, center: ${center}, is_reverse: ${is_reverse}'
+									break
+								}
+							}
+						}
+						if z == 0 {
+							if is_reverse {
+								mut count := 0
+								for test in remove_from_base([1, 2, 3], [center]) {
+									if next[next.len - 1] == test {
+										count += 1
+									}
+								}
+								assert count == 1, 'assertion failed z center pos: ${pos}, next: ${next}, center: ${center}, is_reverse: ${is_reverse}'
+							} else {
+								assert next == [x, y, center], 'assertion failed z = 0 pos: ${pos}, next: ${next}, center: ${center}, is_reverse: ${is_reverse}'
+								break
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	println('success')
+
+	println('Passed')
+	println('--------------------')
+}
+
+// utilitary
+fn test_check_reverse() {
+	println('--------------------')
+	println('test_check_reverse():')
+
+	print('test 1 special case: ')
+	assert check_reverse([0]) == false, 'assertion failed for specific: [0]'
+	assert check_reverse([0, 2]) == true, 'assertion failed for specific: [0]'
+	assert check_reverse([0, 0, 0]) == false, 'assertion failed for specific: [0]'
+	println('success')
+
+	print('test 2: ')
+	mut test := [0]
+	mut value := false
+	for _ in 0 .. 3 {
+		assert check_reverse(test) == value, 'assertion failed for specific: [0]'
+		test << [0]
+		value = !value
+	}
+	println('success')
+
+	println('Passed')
+	println('--------------------')
+}
+
+fn test_remove_from_base() {
+	println('--------------------')
+	println('test_private():')
+
+	print('test 1 usefull: ')
+	base := [0, 1, 2, 3]
+	assert remove_from_base(base, [0]) == [1, 2, 3], 'assertion failed for specific: [0]'
+	assert remove_from_base(base, [1]) == [0, 2, 3], 'assertion failed for specific: [1]'
+	assert remove_from_base(base, [2]) == [0, 1, 3], 'assertion failed for specific: [2]'
+	assert remove_from_base(base, [3]) == [0, 1, 2], 'assertion failed for specific: [3]'
+
+	for i in 0 .. 5 {
+		assert remove_from_base(base, base[..i]).len == base.len - i, 'assertion failed for i: ${i}'
+	}
+	assert remove_from_base(base, []).len == 4, 'assertion failed for empty []'
+	assert remove_from_base(base, [5, 6, 7]).len == 4, 'assertion failed for non in base [5, 6, 7]'
+	assert remove_from_base(base, [3, 3]).len == 3, 'assertion failed for repetitive: [3, 3]'
+	println('success')
+
+	print('test 2 extension: ')
 	n := 10
 	ensemble := []int{len: n, init: index}
-	for elem in ensemble{
-		assert remove_from_base(ensemble, [elem]).len == ensemble.len - 1 , 'assertion failed for elem: ${elem}'
-	} 
-
-	for i in 0..n{
-		assert remove_from_base(ensemble, ensemble[..i]).len == ensemble.len - i , 'assertion failed for i: ${i}'
+	for elem in ensemble {
+		assert remove_from_base(ensemble, [elem]).len == ensemble.len - 1, 'assertion failed for elem: ${elem}'
 	}
-	println("success")
 
-	println("Passed")
-	println("--------------------")
+	for i in 0 .. n {
+		assert remove_from_base(ensemble, ensemble[..i]).len == ensemble.len - i, 'assertion failed for i: ${i}'
+	}
+	println('success')
+
+	println('Passed')
+	println('--------------------')
 }
